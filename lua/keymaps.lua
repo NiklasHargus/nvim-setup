1c00e53a6a3f14a52d162b921e062362de576bc2
Niklas <niklas@hargus.de>
2024-03-18 15:48:10 +0100

    tasdf

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -33,12 +33,28 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.keymap.set('n', '<C-n>', "<cmd>Explore<cr>")
 
 -- Change focus
-vim.keymap.set('n', '<C-h>', '<C-w>h')
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-vim.keymap.set('n', '<C-k>', '<C-w>k')
-vim.keymap.set('n', '<C-l>', '<C-w>l')
+-- vim.keymap.set('n', '<C-h>', '<C-w>h')
+-- vim.keymap.set('n', '<C-j>', '<C-w>j')
+-- vim.keymap.set('n', '<C-k>', '<C-w>k')
+-- vim.keymap.set('n', '<C-l>', '<C-w>l')
 
 -- Splits
 vim.keymap.set('n', '<leader>ws', "<cmd>split<cr>", {desc = "Split Panel Horizontal"})
 vim.keymap.set('n', '<leader>wv', "<cmd>vsplit<cr>", {desc = "Split Panel Vertical"})
 vim.keymap.set('n', '<leader>wq', "<cmd>q<cr>", {desc = "Close Panel"})
+
+
+-- Tmux 
+vim.keymap.set('n', '<C-h>', '<cmd> TmuxNavigateLeft<cr>', {desc = "Move left"})
+vim.keymap.set('n', '<C-j>', '<cmd> TmuxNavigateDown<cr>', {desc = "Move left"})
+vim.keymap.set('n', '<C-k>', '<cmd> TmuxNavigateUp<cr>', {desc = "Move left"})
+vim.keymap.set('n', '<C-l>', '<cmd> TmuxNavigateRight<cr>', {desc = "Move left"})
+
+
+
+
+
+
+
+
+
